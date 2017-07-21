3ec561b43ce32d70d32ca65e099d81ab22f89020
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-07-21 11:54:16 +0500

    addet typeFace with textVIew

--- app/src/main/java/com/example/ee/textstyle/MainActivity.java
+++ app/src/main/java/com/example/ee/textstyle/MainActivity.java
@@ -7,16 +7,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv1,tv2,tv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv1= (TextView)findViewById(R.id.kookaburra_3d);
-        TextView tv2= (TextView)findViewById(R.id.kookaburra_outline);
-        TextView tv3= (TextView)findViewById(R.id.kookobora_regular);
 
-        
+
+        TextView tv1= (TextView)findViewById(R.id.textView9);
+        TextView tv2= (TextView)findViewById(R.id.textView10);
+        TextView tv3= (TextView)findViewById(R.id.textView11);
+        TextView tv5= (TextView)findViewById(R.id.textView12);
+        TextView tv4= (TextView)findViewById(R.id.textView13);
+        TextView tv6= (TextView)findViewById(R.id.textView14);
+
+        Typeface f1 = Typeface.createFromAsset(getAssets(),"Bates Shower.ttf");
+        Typeface f2 = Typeface.createFromAsset(getAssets(),"GoodDog.ttf");
+        Typeface f3 = Typeface.createFromAsset(getAssets(),"GrandHotel-Regular.ttf");
+        Typeface f4 = Typeface.createFromAsset(getAssets(),"KaushanScript-Regular.ttf");
+        Typeface f5 = Typeface.createFromAsset(getAssets(),"Pacifico.ttf");
+        Typeface f6 = Typeface.createFromAsset(getAssets(),"Stackyard_PERSONAL_USE.ttf");
+
+        tv1.setTypeface(f1);
+        tv2.setTypeface(f2);
+        tv3.setTypeface(f3);
+        tv4.setTypeface(f4);
+        tv5.setTypeface(f5);
+        tv6.setTypeface(f6);
+
+
     }
 }
